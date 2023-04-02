927942e3c6195520bdf1bef9fbc9b85fdedacfe1
samervalente <samervalente@gmail.com>
2023-04-02 19:13:41 -0300

    feat: create user models

---
+++ src/test/java/com/aceleracaojavav2/models/User.java
@@ -0,0 +1,33 @@
+package com.aceleracaojavav2.models;
+
+import java.net.URL;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class User {
+    
+    public User(UserDTO userData){
+        this.username = userData.username();
+        this.avatar = userData.avatar();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(length = 50, nullable = false)
+    String username;
+
+    @Column(nullable = false)
+    URL avatar;
+   
+}
